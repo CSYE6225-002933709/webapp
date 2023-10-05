748b99ab1360d5280d201de8546b32bd83a785b9
detectivesokka <dutt.sa@northeastern.edu>
2023-10-05 12:56:24 -0400

    A3: Blah

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -1,5 +1,4 @@
 import { Sequelize } from "sequelize";
-import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load .env file
@@ -7,7 +6,8 @@ dotenv.config(); // Load .env file
 const userName = process.env.DB_USERNAME || "root";
 const password = process.env.DB_PASSWORD || "root";
 
-const  sequelize = new Sequelize("saiDB", userName, password, {
+
+const sequelize = new Sequelize("saiDB", userName, password, {
     host: "127.0.0.1",
     dialect: "mysql",
 });
