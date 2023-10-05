e94e6cbda94184b3ac7ec723909c3bc7e34b6d40
detective-sokka <saidutt1997@gmail.com>
2023-10-05 06:50:20 -0400

    A3: Changes

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -1,37 +1,26 @@
 import { Sequelize } from "sequelize";
 import mysql from "mysql2/promise";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config(); // Load .env file
 
 var userName, password;
 
-if (process.env.DB_USERNAME === '') {
-  
-  userName = "root";
-  password = "root";
 
-} else {
+const userName = process.env.DB_USERNAME || "root";
+const password = process.env.DB_PASSWORD || "root";
 
-  userName = process.env.DB_USERNAME;
-  password = process.env.DB_PASSWORD;
-}
-
-
-// TODO : implement .env
 const connection = await mysql.createConnection({
   user: userName,
   password: password,
 });
 
-await connection.query("CREATE DATABASE IF NOT EXISTS saiDB;").catch((err) => {
-  
-  console.log(err);
-});
+  await connection.query("CREATE DATABASE IF NOT EXISTS saiDB;");
+  connection.end();
 
-const sequelize = new Sequelize("saiDB", userName, password, {
-  host: "localhost",
-  dialect: "mysql",
-});
+  const sequelize = new Sequelize("saiDB", userName, password, {
+    host: "127.0.0.1",
+    dialect: "mysql",
+  });
 
 export default sequelize;
