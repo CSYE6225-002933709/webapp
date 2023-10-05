71486971ce81cf107b7000d687824d73ebf391bd
detectivesokka <dutt.sa@northeastern.edu>
2023-10-05 11:43:30 -0400

    A3: Absolute final commit

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -7,22 +7,29 @@ dotenv.config(); // Load .env file
 const userName = process.env.DB_USERNAME || "root";
 const password = process.env.DB_PASSWORD || "root";
 
+var sequelize = null;
+
+const createSequelizeInstance = () => {
+    
+  sequelize = new Sequelize("saiDB", userName, password, {
+  host: "127.0.0.1",
+  dialect: "mysql",
+  });
+};
+
 await mysql
   .createConnection({
     user: userName,
     password: password,
   })
   .then(async (connection) => {
-    await connection.query("CREATE DATABASE IF NOT EXISTS saiDB;").catch((err) => console.log(err));
-    connection.end();
-  })
-  .catch((err) =>{
-    console.log(err);
-  });
-
-const sequelize = new Sequelize("saiDB", userName, password, {
-  host: "127.0.0.1",
-  dialect: "mysql",
-});
+    await connection.query("CREATE DATABASE IF NOT EXISTS saiDB;")
+    .then(async () => {
+      await createSequelizeInstance();
+    });
+    })
+  .catch((err) => console.log(err));
+  
+  
 
 export default sequelize;
