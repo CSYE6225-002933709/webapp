99eb0240c58637f8c62351eeb070af5ecab573d9
detectivesokka <dutt.sa@northeastern.edu>
2023-10-05 12:02:18 -0400

    Blah

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -7,29 +7,10 @@ dotenv.config(); // Load .env file
 const userName = process.env.DB_USERNAME || "root";
 const password = process.env.DB_PASSWORD || "root";
 
-var sequelize = null;
-
-const createSequelizeInstance = () => {
-  sequelize = new Sequelize("saiDB", userName, password, {
+const  sequelize = new Sequelize("saiDB", userName, password, {
     host: "127.0.0.1",
     dialect: "mysql",
-  });
-};
+});
 
-try {
-
-  const connection = await mysql.createConnection({
-    user: userName,
-    password: password,
-  });
-
-  await connection.query("CREATE DATABASE IF NOT EXISTS saiDB;");
-
-  createSequelizeInstance();
-
-} catch (err) {
-  
-  console.log(err);
-}
 
 export default sequelize;
