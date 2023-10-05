a00539e4023c20ebd763d4562570a309f2a964c3
detectivesokka <dutt.sa@northeastern.edu>
2023-10-05 05:15:37 -0400

    A3: Reading from .env

--- src/configs/sequelize.js
+++ src/configs/sequelize.js
@@ -1,9 +1,23 @@
 import { Sequelize } from "sequelize";
+import mysql from "mysql2/promise";
+import dotenv from 'dotenv';
 
-// TODO : implement .env 
-const sequelize = new Sequelize("saiDB", "root", "The1m@m@", {
-    host: "localhost",
-    dialect: "mysql",
+dotenv.config(); // Load .env file
+
+const userName = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+
+// TODO : implement .env
+const connection = await mysql.createConnection({
+  user: userName,
+  password: password,
+});
+
+await connection.query("CREATE DATABASE IF NOT EXISTS saiDB;");
+
+const sequelize = new Sequelize("saiDB", userName, password, {
+  host: "localhost",
+  dialect: "mysql",
 });
 
 export default sequelize;
